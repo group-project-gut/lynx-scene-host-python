86112116b644f008ebe036972048b0936d45fe98
blazej-smorawski <blazej.smorawski@gmail.com>
2023-03-21 12:13:49 +0100

    Refactor `uvicorn` usage

--- scene-host-python.py
+++ scene-host-python.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 
+import uvicorn
 from src.server import Server
 
 
-def main():
-    server = Server()
-    server.run()
+server = Server()
 
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    uvicorn.run("scene-host-python:server.app", host="0.0.0.0", port=8555, workers=1)
--- src/server.py
+++ src/server.py
@@ -1,22 +1,16 @@
 from fastapi import FastAPI
-import uvicorn
 
 class Server():
-    def __init__(self, host: str = "0.0.0.0", port: int = 8555) -> None:
-        self.host = host
-        self.port = port
-        
-    def run(self) -> None:
-        app = FastAPI()
+    def __init__(self) -> None:
+        self.app = FastAPI()
         context = {'num': 1}
 
-        @app.get("/get")
+        @self.app.get("/get")
         async def get():
             return {"message": context['num']}
         
-        @app.get("/add")
+        @self.app.get("/add")
         async def add():
             context['num'] = context['num'] + 1
             return {"message": context['num']}
-        
-        uvicorn.run(app, host=self.host, port=self.port, workers=1)
+   
