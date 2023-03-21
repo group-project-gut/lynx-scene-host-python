a0c866e1768941e5cde4c2a887c42bd0a10ee945
blazej-smorawski <blazej.smorawski@gmail.com>
2023-03-21 18:24:34 +0100

    Add base for `execution-runtime`

--- scene-host-python.py
+++ scene-host-python.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
 import uvicorn
-from src.server import Server
+from src.scene_server import SceneServer
 
 
-server = Server()
+server = SceneServer()
 
 if __name__ == "__main__":
     uvicorn.run("scene-host-python:server.app", host="0.0.0.0", port=8555, workers=1)
+    for process_data in server.processes:
+        process_data.process.terminate
---
+++ src/scene_server.py
@@ -0,0 +1,57 @@
+from multiprocessing.connection import Connection
+from attr import dataclass
+from fastapi import FastAPI
+from lynx.common.scene import Scene
+from lynx.common.object import Object
+from lynx.common.actions.move import Move
+from lynx.common.vector import Vector
+from pydantic import BaseModel
+from multiprocessing import Pipe, Process
+
+def execution_runtime(pipe: Connection, object_id: int):
+    scene_serialzied = pipe.recv()
+    scene = Scene.deserialize(scene_serialzied)
+    action = Move(object_id=1, vector=Vector(10,10))
+    pipe.send([action.serialize()])
+
+@dataclass
+class ProcessData():
+    process: Process = None
+    pipe: Connection = None
+
+class SceneServer():
+    def __init__(self) -> None:
+        self.app = FastAPI()
+        self.scene = Scene()
+        self.processes = {}
+
+        @self.app.get("/")
+        async def get():
+            return {"scene": self.scene.serialize()}
+
+        class AddObjectRequest(BaseModel):
+            serialized_object: str
+
+        @self.app.post("/add_object")
+        async def add(r: AddObjectRequest):
+            object = Object.deserialize(r.serialized_object)
+            self.scene.add_entity(object)
+
+            if object.tick != "":
+                parent_conn, child_conn = Pipe()
+                p = Process(target=execution_runtime, args=(child_conn, object.id,))
+                p.start()
+                self.processes[object.id] = ProcessData(process=p, pipe=parent_conn)
+
+            return {"serialized_object": object.serialize()}
+
+        @self.app.post("/tick")
+        async def tick():
+            actions = []
+            for process_data in self.processes.values():
+                serialized_scene = self.scene.serialize()
+                process_data.pipe.send(serialized_scene)
+                actions += process_data.pipe.recv()
+
+            return {"actions": actions}
+
