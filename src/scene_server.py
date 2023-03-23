568f4106e05b45620e90fcbfb956724bd861feec
blazej-smorawski <blazej.smorawski@gmail.com>
2023-03-23 09:34:34 +0100

    Async `tick` benchmark

--- src/scene_server.py
+++ src/scene_server.py
@@ -1,25 +1,31 @@
-from multiprocessing.connection import Connection
+import asyncio
+import time
+
+from aioprocessing import AioPipe, AioProcess
+from aioprocessing.connection import AioConnection
 from attr import dataclass
 from fastapi import FastAPI
-from lynx.common.scene import Scene
 from lynx.common.object import Object
-from lynx.common.actions.move import Move
-from lynx.common.vector import Vector
+from lynx.common.scene import Scene
 from pydantic import BaseModel
-from multiprocessing import Pipe, Process
 
 
-def execution_runtime(pipe: Connection, object_id: int):
-    scene_serialzied = pipe.recv()
-    scene = Scene.deserialize(scene_serialzied)
-    action = Move(object_id=1, vector=Vector(10, 10))
-    pipe.send([action.serialize()])
+def execution_runtime(pipe: AioConnection, object_id: int):
+    from lynx.common.actions.move import Move
+    from lynx.common.vector import Vector
+
+    while (True):
+        scene_serialzied = pipe.recv()
+        scene = Scene.deserialize(scene_serialzied)
+        action = Move(object_id=object_id, vector=Vector(10, 10))
+        time.sleep(1)
+        pipe.send([action.serialize()])
 
 
 @dataclass
 class ProcessData():
-    process: Process = None
-    pipe: Connection = None
+    process: AioProcess = None
+    pipe: AioConnection = None
 
 
 class SceneServer():
@@ -38,25 +44,44 @@ class SceneServer():
         @self.app.post("/add_object")
         async def add(r: AddObjectRequest):
             object = Object.deserialize(r.serialized_object)
-            self.scene.add_entity(object)
-
+            # self.scene.add_entity(object)
             if object.tick != "":
-                parent_conn, child_conn = Pipe()
-                p = Process(target=execution_runtime,
-                            args=(child_conn, object.id,))
+                parent_conn, child_conn = AioPipe()
+                p = AioProcess(target=execution_runtime,
+                               args=(child_conn, object.id,))
                 p.start()
                 self.processes[object.id] = ProcessData(
                     process=p, pipe=parent_conn)
 
             return {"serialized_object": object.serialize()}
 
+        async def run_object(serialized_scene: str, process_data: ProcessData):
+            await process_data.pipe.coro_send(serialized_scene)
+            return await process_data.pipe.coro_recv()
+
         @self.app.post("/tick")
         async def tick():
             actions = []
-            # I guess it should be async but no idea how it works with send and recv
+            serialized_scene = self.scene.serialize()
+
+            # Sequential approach
+            start = time.perf_counter()
             for process_data in self.processes.values():
-                serialized_scene = self.scene.serialize()
                 process_data.pipe.send(serialized_scene)
                 actions += process_data.pipe.recv()
+            stop = time.perf_counter()
+            seq_time = stop - start
 
-            return {"actions": actions}
+            # Async approach
+            tasks = []
+            start = time.perf_counter()
+            for process_data in self.processes.values():
+                task = asyncio.create_task(
+                    run_object(serialized_scene, process_data))
+                tasks.append(task)
+
+            actions = await asyncio.gather(*tasks)
+            stop = time.perf_counter()
+            async_time = stop - start
+
+            return {"seq": seq_time, "async": async_time}
