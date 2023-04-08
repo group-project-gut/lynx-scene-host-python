2f3370bb558004e9954b2cd53090109dc6c413dd
FilipSzweda <filip.szweda1@gmail.com>
2023-04-08 16:16:07 +0200

    NOISSUE Change default scene host server IP

--- scene-host-python.py
+++ scene-host-python.py
@@ -7,6 +7,6 @@ from src.scene_server import SceneServer
 server = SceneServer()
 
 if __name__ == "__main__":
-    uvicorn.run("scene-host-python:server.app", host="0.0.0.0", port=8555, workers=1)
+    uvicorn.run("scene-host-python:server.app", host="127.0.0.1", port=8555, workers=1)
     for process_data in server.processes:
         process_data.process.terminate
