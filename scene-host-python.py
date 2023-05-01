9be19f975dfaa8ca1aa8029627390f455f75b690
blazej-smorawski <blazej.smorawski@gmail.com>
2023-05-01 15:45:41 +0200

    Listen on all interfaces

--- scene-host-python.py
+++ scene-host-python.py
@@ -7,5 +7,5 @@ from src.scene_server import SceneServer
 server = SceneServer()
 
 if __name__ == "__main__":
-    uvicorn.run("scene-host-python:server.app", host="127.0.0.1", port=8555, workers=1)
+    uvicorn.run("scene-host-python:server.app", host="0.0.0.0", port=8555, workers=1)
     server.teardown()
